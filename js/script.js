863ba84560feae6d46c5488796b8e3b5ec63497c
Nathan Rossow <nathanrossow@gmail.com>
2024-11-27 23:57:46 -0600

    Update and rename js/script.js to services.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Mobile menu toggle
-const menuToggle = document.getElementById('menu-toggle');
-const navList = document.getElementById('nav-list');
-
-// Toggle the mobile menu visibility
-menuToggle.addEventListener('click', () => {
-  navList.classList.toggle('active');
-});
